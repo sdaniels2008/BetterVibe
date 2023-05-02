292de25f6e3744a6ebd1f7160952809aafa7a4d4
Sebastian Daniels <sebastian.daniels2008@gmail.com>
2023-05-02 14:31:03 +0330

    Add genres

---
+++ pages/genre/[id].js
@@ -0,0 +1,39 @@
+import { useQuery } from "react-query";
+import { useRouter } from 'next/router'
+import Link from 'next/link'
+
+import { fetchGenre } from "../../lib/fetch";
+
+export default function Genre() {
+  const router = useRouter()
+  const { id } = router.query
+
+  const { isLoading, isError, isSuccess, data } = useQuery(['genre', id], () => fetchGenre(id))
+
+
+  return (
+    <div>
+      moo
+
+      {!isLoading && data && <List data={data.data} />}
+    </div>
+  );
+}
+
+function List({ data }) {
+  return <>
+    {Object.keys(data.shelf).map(category => (
+      <div className="p-4">
+        <h2 class="text-lg font-bold">{category}</h2>
+        <ul>
+        {data.shelf[category].map(playlist => (
+          <li key={playlist.id}><Link href={`/playlist/${playlist.id}`}>
+            <img width="60" height="60" src={playlist.thumbnails[0].url} />
+            {playlist.title}
+          </Link></li>
+        ))}
+        </ul>
+      </div>
+    ))}
+  </>
+}
--- pages/index.js
+++ pages/index.js
@@ -1,5 +1,10 @@
 import Head from 'next/head'
+import Link from 'next/link'
 import { Inter } from 'next/font/google'
+import { useState } from "react";
+import { useQuery } from "react-query";
+
+import { fetchSearch, fetchGenres } from "../lib/fetch";
 
 const inter = Inter({ subsets: ['latin'] })
 
@@ -12,7 +17,74 @@ export default function Home() {
         <meta name="viewport" content="width=device-width, initial-scale=1" />
         <link rel="icon" href="/favicon.ico" />
       </Head>
-      <div className="text-3xl font-bold underline">Home Page</div>
+      
+      <Search />
+
+      <Genres />
     </>
+  )
+}
+
+function Genres() {
+  const { isLoading, isError, isSuccess, data } = useQuery('genres', () => fetchGenres())
+
+  if(isLoading) {
+    return 'loading...';
+  }
+
+  return <>
+    <div>
+      <div className="flex flex-wrap gap-2">
+    {data.data.moods.filter(mood => mood.title !== 'Black Lives Matter').map(mood => (
+      <Link href={`/genre/${mood.id}`} className="bg-blue-200 w-20 h-20">
+        {mood.title}
+      </Link>
+    ))}
+    </div>
+
+    <hr className="my-4" />
+
+      <div className="flex flex-wrap gap-2">
+    {data.data.genres.map(genre => (
+        <Link href={`/genre/${genre.id}`} className="bg-gray-200 w-20 h-20">
+        {genre.title}
+      </Link>
+      ))}
+      </div>
+    </div>
+  </>
+}
+
+
+function Search() {
+  const [term, setTerm] = useState('')
+  const { isLoading, isError, isSuccess, data } = useQuery(['search', term], () => fetchSearch(term), {
+    enabled: Boolean(term),
+  })
+
+  return (
+    <div>
+      <div>
+      <input value={term} onChange={e => setTerm(e.target.value)} className="border rounded p-4" placeholder="Album, Song or an Artist..." />
+      </div>
+
+      {!isLoading && data && <List data={data.data} />}
+    </div>
+  );
+}
+
+function List({ data }) {
+  if( ! data.items) {
+    return 'no results :(';
+  }
+
+  return (
+    <ul>
+      {data.items.map(item => <li key={item.url}>
+        <a href={item.url.replace('?list=', '/')}>
+        <img width="60" height="60" src={item.thumbnail} /> {item.name}
+        </a>
+        </li>)}
+    </ul>
   )
 }
